32ee69d267377d06c0f28c6d4e0cff35672cbaa7
Arif <arif.rahman2009@gmail.com>
2017-12-20 10:27:17 +0600

    112 Creating Immutable State

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -39,6 +39,8 @@ class LoginForm extends Component {
 	}
 }
 
-export default connect(null, {
-	emailChanged
-})(LoginForm);
+const mapStateToProps = state => {
+	return { email: state.auth.email };
+};
+
+export default connect(mapStateToProps, { emailChanged })(LoginForm);
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -10,7 +10,7 @@ export default (state = INITIAL_STATE, action) => {
 	console.log(action);
 	switch (action.type) {
 		case EMAIL_CHANGED:
-			
+		return { ...state, email: action.payload };
 		default:
 			return state;
 

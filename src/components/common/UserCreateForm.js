1ab571511475da8c3edbad7945f9bc8cd45ad36d
Arif <arif.rahman2009@gmail.com>
2018-05-13 23:34:41 +0600

    Update

--- src/components/common/UserCreateForm.js
+++ src/components/common/UserCreateForm.js
@@ -31,12 +31,12 @@ const warn = values => {
     return warnings
 }
 
-const renderField = ({ label, keyboardType, meta: { touched, error, warning }, input: { onChange }}) => {
+const renderField = ({ label, keyboardType, meta: { touched, error, warning }, input: { onChange, ...restInput }}) => {
     return (<View style={{ flexDirection: 'column', height: 70, alignItems: 'flex-start' }}>
         <View style={{ flexDirection: 'row', height: 50, alignItems: 'center' }}>
             <Text style={{ fontSize: 14, fontWeight: 'bold', width: 80 }}>{label}</Text>
             <TextInput style={{ borderColor: 'steelblue', borderWidth: 1, height: 37, width: 220, padding: 5 }}
-                keyboardType={keyboardType} onChangeText={onChange}
+                keyboardType={keyboardType} onChangeText={onChange} {...restInput}
             >
             </TextInput>
         </View>

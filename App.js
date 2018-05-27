d92c8d58c0266fd30cb3cdfe6ba47029720f53ba
Arif <arif.rahman2009@gmail.com>
2018-05-27 17:27:34 +0600

    Disable warning message from simulator

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import { YellowBox } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk'
 import renders from './src/reducers';
 import Router from './Router';
+
+YellowBox.ignoreWarnings([
+  'Warning: componentWillMount is deprecated',
+  'Warning: componentWillReceiveProps is deprecated',
+  'Warning: componentWillUpdate is deprecated'
+]);
 
 export default class App extends Component {
   render() {

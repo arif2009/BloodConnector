d8d049b52a2f297def8a7b7a68a77fa83f2ce985
Arif <arif.rahman2009@gmail.com>
2018-06-06 15:19:07 +0600

    Added live origin

--- src/utills/we.js
+++ src/utills/we.js
@@ -1,7 +1,7 @@
 const we = {
     version: '2.5.0',
     twoLetterYear: new Date().getFullYear().toString().substr(-2),
-    apiOrigin: 'http://10.0.2.2/', //'http://www.bloodconnector.org/'
+    apiOrigin: 'http://www.bloodconnector.org/', //'http://10.0.2.2/'
     makeFirstCharecterLower: string => string.charAt(0).toLowerCase() + string.slice(1),
     processModelstateError: function(modelErr){
 

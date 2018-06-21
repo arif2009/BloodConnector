b54bf73fe3f18af2a2b3db3859bad834b649b50a
Arif <arif.rahman2009@gmail.com>
2018-06-21 11:49:00 +0600

    Update styles.js

--- src/components/styles.js
+++ src/components/styles.js
@@ -1,25 +1,15 @@
 'use strict';
 import { StyleSheet } from 'react-native';
 module.exports = StyleSheet.create({
-    bgColor: {backgroundColor: '#ffcccc'},
-
-    hRed:{backgroundColor: '#e60000'},
-
-    nabBg: {backgroundColor: '#ff0000'},
 
     txtColor: {color: '#fff'},
-
     txtBlue:{color: '#0000ff'},
-
     txtRed:{color: '#ff0000'},
-
     txtWarning:{color: '#ffc107'},
-
     txtDanger:{color: '#dc3545'},
-
-    borderBottom0:{borderBottomWidth:0},
-
-    selfAlignCenter:{alignSelf: 'center'},
+    txtMedium: {fontSize: 16,fontWeight: '600'},
+    txtBold: {fontWeight: 'bold'},
+    txtBolder: {fontSize: 18,fontWeight: 'bold'},
 
     p:{padding:10},
     pL:{ paddingLeft:10},
@@ -28,32 +18,30 @@ module.exports = StyleSheet.create({
 
     msm:{margin:5},
     mLg:{margin:15},
-
     mb:{marginBottom: 10},
     mbSm:{marginBottom:4},
     ml:{marginLeft: 10},
-
     mr:{marginRight: 10},
-
     mlLg:{marginLeft: 15},
-
     mrLg:{marginRight: 15},
-
     mt:{marginTop: 10},
-
     mtLg:{marginTop: 15},
+
+    bgColor: {backgroundColor: '#ffcccc'},
+
+    hRed:{backgroundColor: '#e60000'},
+
+    nabBg: {backgroundColor: '#ff0000'},
+
+    borderBottom0:{borderBottomWidth:0},
+
+    selfAlignCenter:{alignSelf: 'center'},
 
     footerBg: {backgroundColor: '#ff8080',justifyContent:'center'},
 
     budgetTxt: {color: '#fff',fontSize: 18,fontWeight: 'bold'},
 
     sceneTitle: {alignSelf: 'center',color: '#fff'},
-
-    txtMedium: {fontSize: 16,fontWeight: '600'},
-
-    txtBold: {fontWeight: 'bold'},
-
-    txtBolder: {fontSize: 18,fontWeight: 'bold'},
 
     drawerContainer: {flex: 1,backgroundColor: '#ffcccc'},
 
@@ -83,7 +71,6 @@ module.exports = StyleSheet.create({
         borderRadius:4, 
         backgroundColor: '#0000ff'
     },
-    itemStyle: {borderBottomWidth: 1,borderColor: '#fff'},
     button: {
         shadowColor: 'rgba(0,0,0, .4)', // IOS
         shadowOffset: { height: 1, width: 1 }, // IOS
@@ -96,19 +83,17 @@ module.exports = StyleSheet.create({
         alignItems: 'center',
         flexDirection: 'row'
     },
-    bgContainer: { flex:1, width: null, height: null },
-    
     modal: {
       justifyContent: 'center', //justifyContent: "center", "space-between", "flex-end", "space-around", "flex-start"
       alignItems: 'center', //'flex-start', 'flex-end', 'center', 'stretch', 'baseline'
       borderRadius:4,
       height: null,
     },
-    tAndCmodal: {
-      width: 300
-    },
-    detailsmodal: {
-        top:150,
-        width: 300
-      }
+    tAndCmodal: { width: 300 },
+
+    detailsmodal: { top:150, width: 300 },
+
+    bgContainer: { flex:1, width: null, height: null },
+
+    itemStyle: {borderBottomWidth: 1,borderColor: '#fff'}
 });

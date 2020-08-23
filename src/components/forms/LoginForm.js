57c0766199ab2ec6e8d0316353da10bb44e41e11
Arif2009 <arif.rahman2009@gmail.com>
2020-08-23 09:58:35 +0600

    Fixed password input

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Field, reduxForm} from 'redux-form';
 import {View, Text, TouchableOpacity} from 'react-native';
 import {Item, Input, Spinner, Icon} from 'native-base';
@@ -31,17 +31,16 @@ const renderField = ({
   keyboardType,
   placeholder,
   meta: {touched, error, warning},
-  input: {onChange, ...restInput},
+  input,
 }) => {
   return (
     <View style={mbSm}>
       <Item error={touched && !!error} rounded>
         <Icon type={iconType} name={iconName} />
         <Input
-          secureTpickerStyleextEntry={JSON.parse(secureTextEntry)}
+          {...input}
+          secureTextEntry={JSON.parse(secureTextEntry)}
           keyboardType={keyboardType}
-          onChangeText={onChange}
-          {...restInput}
           placeholder={placeholder}
           autoCapitalize="none"
         />
@@ -58,53 +57,48 @@ const renderField = ({
   );
 };
 
-class LoginComponent extends Component {
-  constructor(props) {
-    super(props);
-  }
+const LoginComponent = (props) => {
+  const {handleSubmit, submitting, submitFailed, error} = props;
 
-  render() {
-    const {handleSubmit, submitting, reset, submitFailed, error} = this.props;
+  return (
+    <View style={styles.loginContainer}>
+      {submitFailed && (
+        <Text style={[mbSm, txtDanger, selfAlignCenter]}>{error}</Text>
+      )}
 
-    return (
-      <View style={styles.loginContainer}>
-        {submitFailed && (
-          <Text style={[mbSm, txtDanger, selfAlignCenter]}>{error}</Text>
-        )}
+      <Field
+        name="email"
+        iconType="FontAwesome"
+        iconName="envelope-o"
+        secureTextEntry="false"
+        keyboardType="email-address"
+        placeholder="Your email"
+        component={renderField}
+        validate={[required, isValidEmail]}
+        warn={isYahooMail}
+      />
+      <Field
+        name="password"
+        type="password"
+        iconType="SimpleLineIcons"
+        iconName="lock-open"
+        secureTextEntry="true"
+        keyboardType="default"
+        placeholder="Password"
+        component={renderField}
+        validate={[required, minValue6, maxLength12]}
+      />
 
-        <Field
-          name="email"
-          iconType="FontAwesome"
-          iconName="envelope-o"
-          secureTextEntry="false"
-          keyboardType="email-address"
-          placeholder="Your email"
-          component={renderField}
-          validate={[required, isValidEmail]}
-          warn={isYahooMail}
-        />
-        <Field
-          name="password"
-          iconType="SimpleLineIcons"
-          iconName="lock-open"
-          secureTextEntry="true"
-          keyboardType="default"
-          placeholder="Password"
-          component={renderField}
-          validate={[required, minValue6, maxLength12]}
-        />
-
-        <TouchableOpacity
-          disabled={submitting}
-          onPress={handleSubmit(loginUser)}
-          style={[button, mtSm, bgInfo]}>
-          <Text style={[txtColor, txtMedium]}>Submit </Text>
-          {submitting && <Spinner size={25} color="#fff" />}
-        </TouchableOpacity>
-      </View>
-    );
-  }
-}
+      <TouchableOpacity
+        disabled={submitting}
+        onPress={handleSubmit(loginUser)}
+        style={[button, mtSm, bgInfo]}>
+        <Text style={[txtColor, txtMedium]}>Submit </Text>
+        {submitting && <Spinner size={25} color="#fff" />}
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const LoginForm = reduxForm({
   form: USER_LOGIN_FORM, // a unique identifier for this form
